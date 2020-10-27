2093f22bfb14de57b48c54ca705f3dcd7b9ab4b3
igortrevisan <60700923+igortrevisan@users.noreply.github.com>
2020-10-26 21:46:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-mport os
+import os
 from flask import Flask, jsonify, request
 from math import sqrt
 
@@ -6,23 +6,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def sequencia_fibonacci():
-       proximo = 1
-          anterior = 0
-             limite = 100
-                found = 0
-                   resposta = "0,"
-                      while (found < limite):
-                                 found=found+1 
-                                        if (found):
-                                                       tmp = proximo
-                                                                  proximo = proximo + anterior
-                                                                             anterior = tmp
-                                                                                        resposta+= str(proximo) + ","
-                                                                                                   if (found % 5 == 0):
-                                                                                                                      resposta = resposta + "<br>" 
+   proximo = 1
+   anterior = 0
+   limite = 100
+   found = 0
+   resposta = "0,"
+   while (found < limite):
+       found=found+1 
+       if (found):
+           tmp = proximo
+           proximo = proximo + anterior
+           anterior = tmp
+           resposta+= str(proximo) + ","
+           if (found % 5 == 0):
+               resposta = resposta + "<br>" 
 
-                                                                                                                         return resposta
+   return resposta
 
-                                                                                                                     if __name__ == "__main__":
-                                                                                                                             port = int(os.environ.get("PORT", 5000))
-                                                                                                                                 app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
